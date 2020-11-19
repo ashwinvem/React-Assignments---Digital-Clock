88606ce68d16299ee7c32dc31f0ba6d9a03a3aa6
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2020-11-19 10:45:25 +0530

    date comp removed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import Date from "../Date.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clock: new Date().toLocaleTimeString()
+    };
+  }
+  updateClock() {
+    this.setState({
+      clock: new Date().toLocaleTimeString()
+    });
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.updateClock(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
   render() {
     return (
-      <>
-        <Date />
-      </>
+      <div className="Clock">
+        <h3 id="time">{this.state.clock}</h3>
+      </div>
     );
   }
 }
